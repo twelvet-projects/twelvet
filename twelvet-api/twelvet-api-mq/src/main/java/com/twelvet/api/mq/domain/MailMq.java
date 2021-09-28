781b11a05770743a7e0550267956ca78b02db620
twelvet <2471835953@qq.com>
2021-09-28 15:45:16 +0800

    邮件实体

--- twelvet-api/twelvet-api-mq/src/main/java/com/twelvet/api/mq/domain/MailMq.java
+++ twelvet-api/twelvet-api-mq/src/main/java/com/twelvet/api/mq/domain/MailMq.java
@@ -2,6 +2,7 @@ package com.twelvet.api.mq.domain;
 
 import com.twelvet.framework.core.application.domain.BaseEntity;
 
+import java.util.Date;
 import java.util.List;
 
 /**
@@ -43,6 +44,77 @@ public class MailMq extends BaseEntity {
      */
     private List<String> cc;
 
-    
+    /**
+     * 邮件到达时间
+     */
+    private Date sendDate;
 
+    public String getToMail() {
+        return toMail;
+    }
+
+    public void setToMail(String toMail) {
+        this.toMail = toMail;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public boolean isHtml() {
+        return isHtml;
+    }
+
+    public void setHtml(boolean html) {
+        isHtml = html;
+    }
+
+    public List<String> getBcc() {
+        return bcc;
+    }
+
+    public void setBcc(List<String> bcc) {
+        this.bcc = bcc;
+    }
+
+    public List<String> getCc() {
+        return cc;
+    }
+
+    public void setCc(List<String> cc) {
+        this.cc = cc;
+    }
+
+    public Date getSendDate() {
+        return sendDate;
+    }
+
+    public void setSendDate(Date sendDate) {
+        this.sendDate = sendDate;
+    }
+
+    @Override
+    public String toString() {
+        return "MailMq{" +
+                "toMail='" + toMail + '\'' +
+                ", subject='" + subject + '\'' +
+                ", text='" + text + '\'' +
+                ", isHtml=" + isHtml +
+                ", bcc=" + bcc +
+                ", cc=" + cc +
+                ", sendDate=" + sendDate +
+                '}';
+    }
 }
