39016206722805048e03b9fd1cddde65ed140657
twelvet <2471835953@qq.com>
2021-11-23 11:00:28 +0800

    es

---
+++ twelvet-server/twelvet-server-es/src/main/java/com/twelvet/server/es/EsApplication.java
@@ -0,0 +1,22 @@
+package com.twelvet.server.es;
+
+import com.twelvet.framework.core.annotation.EnableTWTFeignClients;
+import com.twelvet.framework.core.annotation.EnableTwelveTConfig;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author twelvet
+ * <p>
+ * 启动入口
+ */
+@EnableTwelveTConfig
+@EnableTWTFeignClients
+@SpringBootApplication
+public class EsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EsApplication.class, args);
+    }
+
+}
