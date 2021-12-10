02cb30e719532e36cf2afa41e82bf01ee15a691c
twelvet <2471835953@qq.com>
2021-12-10 16:47:24 +0800

    netty

--- twelvet-server/twelvet-server-netty/src/main/java/com/twelvet/server/netty/NettyApplication.java
+++ twelvet-server/twelvet-server-netty/src/main/java/com/twelvet/server/netty/NettyApplication.java
@@ -1,89 +1,52 @@
 package com.twelvet.server.netty;
 
 import io.netty.bootstrap.ServerBootstrap;
-import io.netty.channel.*;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.EventLoopGroup;
 import io.netty.channel.nio.NioEventLoopGroup;
-import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
-import io.netty.handler.codec.http.*;
-import io.netty.handler.codec.http.websocketx.extensions.compression.WebSocketServerCompressionHandler;
-import io.netty.handler.stream.ChunkedWriteHandler;
+import io.netty.handler.logging.LogLevel;
+import io.netty.handler.logging.LoggingHandler;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.BeansException;
-import org.springframework.boot.ApplicationArguments;
-import org.springframework.boot.ApplicationRunner;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.ApplicationContextAware;
-import org.springframework.context.ApplicationListener;
-import org.springframework.context.event.ContextClosedEvent;
 import org.springframework.stereotype.Component;
-
-import java.net.InetSocketAddress;
-import java.util.HashMap;
 
 /**
  * @author twelvet
  * <p>
- * 启动服务
+ * 启动Netty服务
  */
 @Component
-public class NettyApplication implements ApplicationRunner, ApplicationListener<ContextClosedEvent>, ApplicationContextAware {
+public class NettyApplication {
 
     private final static Logger log = LoggerFactory.getLogger(NettyApplication.class);
 
-    @Override
-    public void run(ApplicationArguments args) throws InterruptedException {
-        EventLoopGroup bossGroup = new NioEventLoopGroup();
+    public void init() {
+        //bossGroup连接线程组，主要负责接受客户端连接，一般一个线程足矣
+        EventLoopGroup bossGroup = new NioEventLoopGroup(1);
+        //workerGroup工作线程组，主要负责网络IO读写
         EventLoopGroup workerGroup = new NioEventLoopGroup();
         try {
-            ServerBootstrap serverBootstrap = new ServerBootstrap();
-            serverBootstrap.group(bossGroup, workerGroup);
-            serverBootstrap.channel(NioServerSocketChannel.class);
-            serverBootstrap.localAddress(new InetSocketAddress("0.0.0.0", 1024));
-            serverBootstrap.childHandler(new ChannelInitializer<SocketChannel>() {
-                @Override
-                protected void initChannel(SocketChannel socketChannel) {
-                    ChannelPipeline pipeline = socketChannel.pipeline();
-                    pipeline.addLast(new HttpServerCodec());
-                    pipeline.addLast(new ChunkedWriteHandler());
-                    pipeline.addLast(new HttpObjectAggregator(65536));
-                    pipeline.addLast(new ChannelInboundHandlerAdapter() {
-                        @Override
-                        public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
-                            if (msg instanceof FullHttpRequest) {
-                                FullHttpRequest fullHttpRequest = (FullHttpRequest) msg;
-                                String uri = fullHttpRequest.uri();
-                                if (!"/channel".equals(uri)) {
-                                    // 访问的路径不是 websocket的端点地址，响应404
-                                    ctx.channel().writeAndFlush(new DefaultFullHttpResponse(HttpVersion.HTTP_1_1, HttpResponseStatus.NOT_FOUND))
-                                            .addListener(ChannelFutureListener.CLOSE);
-                                    return;
-                                }
-                            }
-                            super.channelRead(ctx, msg);
-                        }
-                    });
-                    pipeline.addLast(new WebSocketServerCompressionHandler());
 
-                }
-            });
-            Channel channel = serverBootstrap.bind().sync().channel();
-            log.info("websocket 服务启动");
-            channel.closeFuture().sync();
+            //启动辅助类
+            ServerBootstrap serverBootstrap = new ServerBootstrap();
+            //bootstrap绑定两个线程组
+            serverBootstrap.group(bossGroup, workerGroup);
+            //设置通道为NioChannel
+            serverBootstrap.channel(NioServerSocketChannel.class);
+            //可以对入站\出站事件进行日志记录，从而方便我们进行问题排查。
+            serverBootstrap.handler(new LoggingHandler(LogLevel.INFO));
+            //启动服务器,本质是Java程序发起系统调用，然后内核底层起了一个处于监听状态的服务，生成一个文件描述符FD
+            ChannelFuture channelFuture = serverBootstrap.bind(2020).sync();
+            //异步
+            channelFuture.channel().closeFuture().sync();
+            log.info("Netty 启动成功");
+        } catch (InterruptedException interruptedException) {
+            interruptedException.printStackTrace();
         } finally {
             bossGroup.shutdownGracefully();
             workerGroup.shutdownGracefully();
         }
     }
 
-    @Override
-    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
-        log.info("websocket 服务启动");
-    }
-
-    @Override
-    public void onApplicationEvent(ContextClosedEvent contextClosedEvent) {
-        log.info("Netty 安全关闭");
-    }
 }
