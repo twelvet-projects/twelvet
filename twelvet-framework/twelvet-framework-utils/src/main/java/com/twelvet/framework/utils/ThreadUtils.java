4f2c861801a35886cd0364f97d27d5650ff9856f
twelvet <2471835953@qq.com>
2021-12-31 15:40:09 +0800

    线程工具类

---
+++ twelvet-framework/twelvet-framework-utils/src/main/java/com/twelvet/framework/utils/ThreadUtils.java
@@ -0,0 +1,39 @@
+package com.twelvet.framework.utils;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @author twelvet
+ * @WebSite www.twelvet.cn
+ * @Description: 线程工具类
+ */
+public class ThreadUtils {
+
+	/**
+	 * Thread sleep
+	 *
+	 * @param millis 时长
+	 */
+	public static void sleep(long millis) {
+		try {
+			Thread.sleep(millis);
+		} catch (InterruptedException e) {
+			Thread.currentThread().interrupt();
+		}
+	}
+
+	/**
+	 * Thread sleep
+	 *
+	 * @param timeUnit TimeUnit
+	 * @param timeout timeout
+	 */
+	public static void sleep(TimeUnit timeUnit, long timeout) {
+		try {
+			timeUnit.sleep(timeout);
+		} catch (InterruptedException e) {
+			Thread.currentThread().interrupt();
+		}
+	}
+
+}
