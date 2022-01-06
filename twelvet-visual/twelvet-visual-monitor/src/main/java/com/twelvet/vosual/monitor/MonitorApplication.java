1b5f564d405eb07c64c2397d01a9a02d20b725bb
twelvet <2471835953@qq.com>
2022-01-06 09:45:43 +0800

    修复服务监控

--- twelvet-visual/twelvet-visual-monitor/src/main/java/com/twelvet/vosual/monitor/MonitorApplication.java
+++ twelvet-visual/twelvet-visual-monitor/src/main/java/com/twelvet/vosual/monitor/MonitorApplication.java
@@ -3,12 +3,14 @@ package com.twelvet.vosual.monitor;
 import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * @author twelvet
  * @WebSite www.twelvet.cn
  * @Description: 监控中心启动器
  */
+@EnableDiscoveryClient
 @EnableAdminServer
 @SpringBootApplication
 public class MonitorApplication {
---
+++ twelvet-visual/twelvet-visual-monitor/src/main/java/com/twelvet/vosual/monitor/config/NacosServiceInstanceConverter.java
@@ -0,0 +1,34 @@
+package com.twelvet.vosual.monitor.config;
+
+import de.codecentric.boot.admin.server.cloud.discovery.DefaultServiceInstanceConverter;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import static java.util.Collections.emptyMap;
+
+/**
+ * @author twelvet
+ * @WebSite www.twelvet.cn
+ * @Description: 针对 nacos 2.x 服务注册处理
+ */
+@Configuration(proxyBeanMethods = false)
+public class NacosServiceInstanceConverter extends DefaultServiceInstanceConverter {
+
+    /**
+     * 重写方法
+     *
+     * @param instance ServiceInstance
+     * @return Map<String, String>
+     */
+    @Override
+    protected Map<String, String> getMetadata(ServiceInstance instance) {
+        return (instance.getMetadata() != null)
+                ? instance.getMetadata().entrySet().stream().filter((e) -> e.getKey() != null && e.getValue() != null)
+                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue))
+                : emptyMap();
+    }
+
+}
