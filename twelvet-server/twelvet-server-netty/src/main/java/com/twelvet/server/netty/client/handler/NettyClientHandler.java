6adf180d4250ed7e69022b7028dfde2d02fd46dd
twelvet <2471835953@qq.com>
2022-01-10 14:27:37 +0800

    完善netty

--- twelvet-server/twelvet-server-netty/src/main/java/com/twelvet/server/netty/client/handler/NettyClientHandler.java
+++ twelvet-server/twelvet-server-netty/src/main/java/com/twelvet/server/netty/client/handler/NettyClientHandler.java
@@ -1,40 +1,56 @@
 package com.twelvet.server.netty.client.handler;
 
+import com.twelvet.server.netty.server.NettyServer;
 import io.netty.buffer.ByteBuf;
 import io.netty.buffer.Unpooled;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
 import io.netty.util.CharsetUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * @author twelvet
  * @WebSite www.twelvet.cn
- * @Description: Netty客户端
+ * @Description: Netty客户端自定义事件处理
  */
 public class NettyClientHandler extends ChannelInboundHandlerAdapter {
 
+    private final static Logger log = LoggerFactory.getLogger(NettyClientHandler.class);
+
+    /**
+     * 读取服务端发送的信息
+     *
+     * @param ctx ChannelHandlerContext
+     */
     @Override
-    public void channelActive(ChannelHandlerContext ctx) throws Exception {
-        System.out.println("client = " + ctx);
+    public void channelActive(ChannelHandlerContext ctx) {
+        log.info("client = {}", ctx);
         ctx.writeAndFlush(Unpooled.copiedBuffer("hello server", CharsetUtil.UTF_8));
     }
 
     /**
      * 通道有读取事件时，会触发
-     * @param ctx
-     * @param msg
-     * @throws Exception
+     *
+     * @param ctx ChannelHandlerContext
+     * @param msg Object
      */
     @Override
-    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+    public void channelRead(ChannelHandlerContext ctx, Object msg) {
         ByteBuf buf = (ByteBuf) msg;
-        System.out.println("服务器回复的消息"+ buf.toString(CharsetUtil.UTF_8));
-        System.out.println("服务器地址" + ctx.channel().remoteAddress());
+        log.info("服务器回复的消息：{}", buf.toString(CharsetUtil.UTF_8));
+        log.info("服务器地址：{}", ctx.channel().remoteAddress());
     }
 
+    /**
+     * 捕获异常
+     *
+     * @param ctx   ChannelHandlerContext
+     * @param cause Throwable
+     */
     @Override
-    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
-        cause.printStackTrace();
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
+        log.error("netty服务链接异常：", cause);
         ctx.close();
     }
 }
--- twelvet-server/twelvet-server-netty/src/main/java/com/twelvet/server/netty/server/handler/NettyServerHandler.java
+++ twelvet-server/twelvet-server-netty/src/main/java/com/twelvet/server/netty/server/handler/NettyServerHandler.java
@@ -1,40 +1,56 @@
 package com.twelvet.server.netty.server.handler;
 
+import com.twelvet.server.netty.client.handler.NettyClientHandler;
 import io.netty.buffer.ByteBuf;
 import io.netty.buffer.Unpooled;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
 import io.netty.util.CharsetUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * @author twelvet
  * @WebSite www.twelvet.cn
- * @Description:
+ * @Description: Netty服务端自定义事件处理
  */
 public class NettyServerHandler extends ChannelInboundHandlerAdapter {
 
+    private final static Logger log = LoggerFactory.getLogger(NettyServerHandler.class);
+
+    /**
+     * 读取客户端发送数据
+     *
+     * @param ctx ChannelHandlerContext
+     * @param msg Object
+     */
     @Override
-    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
-        System.out.println("server ctx =" + ctx);
+    public void channelRead(ChannelHandlerContext ctx, Object msg) {
+        log.info("server ctx = {}", ctx);
         ByteBuf buf = (ByteBuf) msg;
-        System.out.println("客户端发送的消息是"+ buf.toString(CharsetUtil.UTF_8));
+        log.info("客户端发送的消息是：{}", buf.toString(CharsetUtil.UTF_8));
     }
 
     /**
      * 数据读取完毕后执行
      *
-     * @param ctx
-     * @throws Exception
+     * @param ctx ChannelHandlerContext
      */
     @Override
-    public void channelReadComplete(ChannelHandlerContext ctx) throws Exception {
-        System.out.println("收到信息，服务器返回信息");
+    public void channelReadComplete(ChannelHandlerContext ctx) {
+        log.info("收到信息，服务器返回信息");
         ctx.writeAndFlush(Unpooled.copiedBuffer("hello 客户端", CharsetUtil.UTF_8));
     }
 
+    /**
+     * 异常信息关闭
+     *
+     * @param ctx   ChannelHandlerContext
+     * @param cause Throwable
+     */
     @Override
-    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
-        cause.printStackTrace();
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
+        log.error("netty服务链接异常：", cause);
         ctx.close();
     }
 }
