b0f8cd5011758cd96303922123282b01dd85e3bd
twelvet <2471835953@qq.com>
2022-03-18 18:10:20 +0800

    redis测试

--- twelvet-server/twelvet-server-system/src/test/java/com/twelvet/RedissonTest.java
+++ twelvet-server/twelvet-server-system/src/test/java/com/twelvet/RedissonTest.java
@@ -1,13 +1,18 @@
 package com.twelvet;
 
+import org.junit.runner.RunWith;
 import org.redisson.api.RedissonClient;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 /**
  * @author twelvet
  * <p>
  * 测试分布式可重入锁
  */
+@SpringBootTest(classes = SystemApplication.class)
+@RunWith(SpringJUnit4ClassRunner.class)
 public class RedissonTest {
 
     @Autowired
