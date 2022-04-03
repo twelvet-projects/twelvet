574013f88284f03f76c078ad0ca087ad609fd6f6
twelvet <2471835953@qq.com>
2022-04-03 17:47:47 +0800

    redis缓存管理

--- twelvet-framework/twelvet-framework-redis/src/main/java/com/twelvet/framework/redis/service/configure/RedisConfig.java
+++ twelvet-framework/twelvet-framework-redis/src/main/java/com/twelvet/framework/redis/service/configure/RedisConfig.java
@@ -7,7 +7,6 @@ import com.twelvet.framework.redis.service.constants.CacheConstants;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.CachingConfigurerSupport;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.cache.interceptor.KeyGenerator;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
@@ -15,7 +14,6 @@ import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.cache.RedisCacheWriter;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
-import org.springframework.data.redis.serializer.RedisSerializationContext;
 
 import java.time.Duration;
 import java.util.HashMap;
