f99553f552fc7778aa958f29c1dc409209ad5f88
twelvet <2471835953@qq.com>
2022-05-15 22:18:49 +0800

    切换rocketmq

--- twelvet-server/twelvet-server-mq/src/main/java/com/twelvet/server/mq/service/impl/SysLoginLogServiceImpl.java
+++ twelvet-server/twelvet-server-mq/src/main/java/com/twelvet/server/mq/service/impl/SysLoginLogServiceImpl.java
@@ -1,44 +1,36 @@
-/*
+
 package com.twelvet.server.mq.service.impl;
 
-import com.twelvet.api.mq.constant.MQExchangeConstants;
-import com.twelvet.api.mq.constant.MQRoutingKeyConstants;
+import com.twelvet.api.mq.constant.MQGroupConstants;
 import com.twelvet.api.system.domain.SysLoginInfo;
-import com.twelvet.framework.utils.JacksonUtils;
 import com.twelvet.server.mq.service.SysLoginLogService;
-import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.apache.rocketmq.spring.core.RocketMQTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-*/
 /**
  * @author twelvet
  * <p>
  * 邮件业务层实现
- *//*
-
+ */
 @Service
 public class SysLoginLogServiceImpl implements SysLoginLogService {
 
     @Autowired
-    private RabbitTemplate rabbitTemplate;
+    private RocketMQTemplate rocketMQTemplate;
 
-    */
-/**
+    /**
      * 发送系统登录日志MQ
      *
      * @param sysLoginInfo SysLoginInfo
-     *//*
-
+     */
     @Override
     public void sendSysLoginLog(SysLoginInfo sysLoginInfo) {
-        rabbitTemplate.convertAndSend(
-                // 交换机名称
-                MQExchangeConstants.DIRECT_LOG_LOGIN,
-                // 路由
-                MQRoutingKeyConstants.QUEUE_LOG_LOGIN,
-                JacksonUtils.toJson(sysLoginInfo)
+        rocketMQTemplate.convertAndSend(
+                // topic
+                MQGroupConstants.QUEUE_LOG_LOGIN,
+                sysLoginInfo
         );
     }
 }
-*/
+
--- twelvet-server/twelvet-server-mq/src/main/java/com/twelvet/server/mq/service/impl/SysOperationLogServiceImpl.java
+++ twelvet-server/twelvet-server-mq/src/main/java/com/twelvet/server/mq/service/impl/SysOperationLogServiceImpl.java
@@ -1,40 +1,36 @@
-/*
+
 package com.twelvet.server.mq.service.impl;
-
-import com.twelvet.api.mq.constant.MQRoutingKeyConstants;
-import com.twelvet.api.system.domain.SysOperationLog;
-import com.twelvet.framework.utils.JacksonUtils;
-import com.twelvet.server.mq.service.SysOperationLogService;
-import org.springframework.amqp.rabbit.core.RabbitTemplate;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
-*/
 /**
  * @author twelvet
  * <p>
  * 系统操作日志业务层实现
- *//*
+ */
+import com.twelvet.api.mq.constant.MQGroupConstants;
+import com.twelvet.api.system.domain.SysOperationLog;
+import com.twelvet.framework.utils.JacksonUtils;
+import com.twelvet.server.mq.service.SysOperationLogService;
+import org.apache.rocketmq.spring.core.RocketMQTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 @Service
 public class SysOperationLogServiceImpl implements SysOperationLogService {
 
     @Autowired
-    private RabbitTemplate rabbitTemplate;
+    private RocketMQTemplate rocketMQTemplate;
 
-    */
-/**
+
+    /**
      * 发送系统操作日志MQ
      *
      * @param sysOperationLog SysLoginInfo
-     *//*
-
+     */
     @Override
     public void sendSysOperationLog(SysOperationLog sysOperationLog) {
-        rabbitTemplate.convertAndSend(
-                MQRoutingKeyConstants.QUEUE_LOG_OPERATION,
+        rocketMQTemplate.convertAndSend(
+                MQGroupConstants.QUEUE_LOG_OPERATION,
                 JacksonUtils.toJson(sysOperationLog)
         );
     }
 }
-*/
+
