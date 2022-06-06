2e56cd26430b78b4d9e1ad1b0f8b366c68b6e4aa
twelvet <2471835953@qq.com>
2022-06-06 16:17:42 +0800

    接入第三方登录Oauth2 Client

--- twelvet-auth/src/main/java/com/twelvet/AuthApplication.java
+++ twelvet-auth/src/main/java/com/twelvet/AuthApplication.java
@@ -4,6 +4,7 @@ import com.twelvet.framework.core.annotation.EnableTwelveTConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableOAuth2Client;
 
 /**
  * @author twelvet
@@ -14,6 +15,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @EnableTwelveTConfig
 @EnableFeignClients
 @SpringBootApplication
+@EnableOAuth2Client
 public class AuthApplication {
 
     public static void main(String[] args) {
