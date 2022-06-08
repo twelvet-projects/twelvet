15ad0a435209e2c928295fd811325ee1da82479b
twelvet <2471835953@qq.com>
2022-06-08 22:32:03 +0800

    Canal

---
+++ twelvet-server/twelvet-server-es/src/main/java/com/twelvet/server/es/service/impl/CanalServiceImpl.java
@@ -0,0 +1,15 @@
+package com.twelvet.server.es.service.impl;
+
+import com.alibaba.otter.canal.client.CanalConnector;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ * @author twelvet
+ * @WebSite www.twelvet.cn
+ * @Description: 、Canal同步服务
+ */
+@Service
+public class CanalServiceImpl {
+
+}
