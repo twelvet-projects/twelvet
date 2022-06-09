b46f720cb020e2546505ad167d3ec19c9c2d2bcf
TwelveT <2471835953@qq.com>
2022-06-09 18:34:19 +0800

    Authorization Server

--- twelvet-framework/twelvet-framework-security/src/main/java/com/twelvet/framework/security/config/AuthIgnoreConfig.java
+++ twelvet-framework/twelvet-framework-security/src/main/java/com/twelvet/framework/security/config/AuthIgnoreConfig.java
@@ -4,22 +4,17 @@ import cn.hutool.extra.spring.SpringUtil;
 import com.twelvet.framework.security.annotation.AuthIgnore;
 import com.twelvet.framework.security.config.properties.IgnoreUrlsProperties;
 import com.twelvet.framework.utils.$;
-import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.ApplicationContextAware;
 import org.springframework.web.method.HandlerMethod;
-import org.springframework.web.servlet.mvc.condition.PatternsRequestCondition;
 import org.springframework.web.servlet.mvc.method.RequestMappingInfo;
 import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping;
-import org.springframework.web.util.pattern.PathPattern;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
 
 /**
  * @author twelvet
@@ -27,57 +22,49 @@ import java.util.Set;
  * @Description: 忽略服务间的认证
  */
 @Configurable
-public class AuthIgnoreConfig implements InitializingBean, ApplicationContextAware {
+public class AuthIgnoreConfig implements InitializingBean {
 
-	/**
-	 * 注解urls
-	 */
-	@Autowired
-	private IgnoreUrlsProperties ignoreUrlsProperties;
+    /**
+     * 注解urls
+     */
+    @Autowired
+    private IgnoreUrlsProperties ignoreUrlsProperties;
 
-	@Autowired
-	private ApplicationContext applicationContext;
+    private List<String> ignoreUrls = new ArrayList<>();
 
-	private List<String> ignoreUrls = new ArrayList<>();
+    public List<String> getIgnoreUrls() {
+        return ignoreUrls;
+    }
 
-	public List<String> getIgnoreUrls() {
-		return ignoreUrls;
-	}
+    public void setIgnoreUrls(List<String> ignoreUrls) {
+        this.ignoreUrls = ignoreUrls;
+    }
 
-	public void setIgnoreUrls(List<String> ignoreUrls) {
-		this.ignoreUrls = ignoreUrls;
-	}
+    /**
+     * 重写bean注入后
+     */
+    @Override
+    public void afterPropertiesSet() {
+        RequestMappingHandlerMapping mapping = SpringUtil.getBean("requestMappingHandlerMapping");
+        Map<RequestMappingInfo, HandlerMethod> map = mapping.getHandlerMethods();
 
-	/**
-	 * 重写bean注入后
-	 */
-	@Override
-	public void afterPropertiesSet() {
-		RequestMappingHandlerMapping mapping = SpringUtil.getBean("requestMappingHandlerMapping");
-		Map<RequestMappingInfo, HandlerMethod> map = mapping.getHandlerMethods();
+        // 遍历所有mapping
+        map.keySet().forEach(mappingInfo -> {
 
-		// 遍历所有mapping
-		map.keySet().forEach(mappingInfo -> {
+            HandlerMethod handlerMethod = map.get(mappingInfo);
+            // 检测方法是否存在注解
+            AuthIgnore method = handlerMethod.getMethod().getAnnotation(AuthIgnore.class);
+            // 检测Controller是否在注解
+            AuthIgnore controller = handlerMethod.getBeanType().getAnnotation(AuthIgnore.class);
 
-			HandlerMethod handlerMethod = map.get(mappingInfo);
-			// 检测方法是否存在注解
-			AuthIgnore method = handlerMethod.getMethod().getAnnotation(AuthIgnore.class);
-			// 检测Controller是否在注解
-			AuthIgnore controller = handlerMethod.getBeanType().getAnnotation(AuthIgnore.class);
+            // 本方法或本Controller存在AuthIgnore注解将存进列表
+            if ($.isNotEmpty(method) || $.isNotEmpty(controller)) {
+                ignoreUrls.addAll(mappingInfo.getPatternsCondition().getPatterns());
+            }
+        });
 
-			// 本方法或本Controller存在AuthIgnore注解将存进列表
-			if ($.isNotEmpty(method) || $.isNotEmpty(controller)) {
-				ignoreUrls.addAll(mappingInfo.getPatternsCondition().getPatterns());
-			}
-		});
-
-		// 合并放行路径
-		ignoreUrls.addAll(ignoreUrlsProperties.getUrls());
-	}
-
-	@Override
-	public void setApplicationContext(ApplicationContext context) throws BeansException {
-		this.applicationContext = context;
-	}
+        // 合并放行路径
+        ignoreUrls.addAll(ignoreUrlsProperties.getUrls());
+    }
 
 }
