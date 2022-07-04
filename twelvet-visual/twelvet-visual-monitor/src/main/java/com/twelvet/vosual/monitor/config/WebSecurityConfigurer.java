7899e178a0ccca8b672b51027fb88d7e0d270e71
TwelveT <2471835953@qq.com>
2022-07-04 09:39:32 +0800

    升级Spring Boot并移除ES、MQ、Netty

--- twelvet-visual/twelvet-visual-monitor/src/main/java/com/twelvet/vosual/monitor/config/WebSecurityConfigurer.java
+++ twelvet-visual/twelvet-visual-monitor/src/main/java/com/twelvet/vosual/monitor/config/WebSecurityConfigurer.java
@@ -1,9 +1,12 @@
 package com.twelvet.vosual.monitor.config;
 
 import de.codecentric.boot.admin.server.config.AdminServerProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
 
 /**
@@ -11,27 +14,37 @@ import org.springframework.security.web.authentication.SavedRequestAwareAuthenti
  * @WebSite www.twelvet.cn
  * @Description: 监控权限配置
  */
-@Configuration
-public class WebSecurityConfigurer extends WebSecurityConfigurerAdapter {
+@EnableWebSecurity
+public class WebSecurityConfigurer {
+    private final String adminContextPath;
 
-	private final String adminContextPath;
+    public WebSecurityConfigurer(AdminServerProperties adminServerProperties) {
+        this.adminContextPath = adminServerProperties.getContextPath();
+    }
 
-	public WebSecurityConfigurer(AdminServerProperties adminServerProperties) {
-		this.adminContextPath = adminServerProperties.getContextPath();
-	}
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+        SavedRequestAwareAuthenticationSuccessHandler successHandler = new SavedRequestAwareAuthenticationSuccessHandler();
+        successHandler.setTargetUrlParameter("redirectTo");
+        successHandler.setDefaultTargetUrl(adminContextPath + "/");
 
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
-		SavedRequestAwareAuthenticationSuccessHandler successHandler = new SavedRequestAwareAuthenticationSuccessHandler();
-		successHandler.setTargetUrlParameter("redirectTo");
-		successHandler.setDefaultTargetUrl(adminContextPath + "/");
-
-		http.headers().frameOptions().disable().and().authorizeRequests()
-				.antMatchers(adminContextPath + "/assets/**", adminContextPath + "/login",
-						adminContextPath + "/actuator/**", adminContextPath + "/instances/**")
-				.permitAll().anyRequest().authenticated().and().formLogin().loginPage(adminContextPath + "/login")
-				.successHandler(successHandler).and().logout().logoutUrl(adminContextPath + "/logout").and().httpBasic()
-				.and().csrf().disable();
-	}
-
+        return httpSecurity
+                .headers().frameOptions().disable()
+                .and().authorizeRequests()
+                .antMatchers(adminContextPath + "/assets/**"
+                        , adminContextPath + "/login"
+                        , adminContextPath + "/actuator/**"
+                        , adminContextPath + "/instances/**"
+                ).permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin().loginPage(adminContextPath + "/login")
+                .successHandler(successHandler).and()
+                .logout().logoutUrl(adminContextPath + "/logout")
+                .and()
+                .httpBasic().and()
+                .csrf()
+                .disable()
+                .build();
+    }
 }
