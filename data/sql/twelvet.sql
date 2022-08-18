2d0e402c89e6d8259b39b536a2115900a144e9c5
TwelveT <2471835953@qq.com>
2022-08-18 10:09:05 +0800

    dockerfile

--- data/sql/twelvet.sql
+++ data/sql/twelvet.sql
@@ -1,11 +1,4 @@
-DROP DATABASE IF EXISTS `twelvet`;
-
-CREATE DATABASE  `twelvet` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
-
 SET NAMES utf8mb4;
-SET FOREIGN_KEY_CHECKS = 0;
-
-USE `twelvet`;
 -- ----------------------------
 -- Table structure for gen_table
 -- ----------------------------
