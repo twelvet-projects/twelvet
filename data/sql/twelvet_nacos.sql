190dfb468aa19ce6e718b0342d227373612df846
TwelveT <2471835953@qq.com>
2022-08-18 10:05:10 +0800

    dockerfile

--- data/sql/twelvet_nacos.sql
+++ data/sql/twelvet_nacos.sql
@@ -1,3 +1,12 @@
+DROP DATABASE IF EXISTS `twelvet_nacos`;
+
+CREATE DATABASE  `twelvet_nacos` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+USE `twelvet_nacos`;
+
 -- ----------------------------
 -- Table structure for config_info
 -- ----------------------------
