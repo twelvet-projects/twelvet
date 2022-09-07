8164c9e5833e540654a339ed7109b4b01f7786aa
赵怡宁 <2547702006@qq.com>
2022-09-07 21:45:52 +0800

    fix:dfs

--- twelvet-server/twelvet-server-dfs/src/main/java/com/twelvet/server/dfs/controller/DFSController.java
+++ twelvet-server/twelvet-server-dfs/src/main/java/com/twelvet/server/dfs/controller/DFSController.java
@@ -9,6 +9,8 @@ import com.twelvet.framework.jdbc.web.utils.PageUtils;
 import com.twelvet.framework.log.annotation.Log;
 import com.twelvet.framework.log.enums.BusinessType;
 import com.twelvet.server.dfs.service.IDFSService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
@@ -21,6 +23,7 @@ import java.util.List;
  * @WebSite www.twelvet.cn
  * @Description: 文件请求处理
  */
+@Api(value = "DFSController", tags = { "文件请求处理" })
 @RestController
 public class DFSController extends TWTController {
 
@@ -32,6 +35,7 @@ public class DFSController extends TWTController {
 	 * @param files MultipartFile[]
 	 * @return JsonResult<List<SysDfs>>
 	 */
+	@ApiOperation(value = "多文件上传")
 	@Log(service = "多文件上传", businessType = BusinessType.IMPORT)
 	@PostMapping("/batchUpload")
 	public JsonResult<List<SysDfs>> batchUpload(MultipartFile[] files) {
@@ -46,6 +50,7 @@ public class DFSController extends TWTController {
 	 * @param file MultipartFile
 	 * @return JsonResult<String>
 	 */
+	@ApiOperation(value = "单文件上传")
 	@PostMapping("/commonUpload")
 	@Log(service = "单文件上传", businessType = BusinessType.IMPORT)
 	public JsonResult<String> commonUpload(MultipartFile file) {
@@ -55,10 +60,11 @@ public class DFSController extends TWTController {
 	}
 
 	/**
-	 * 删除文件
+	 * 分页查询
 	 * @param fileIds 文件地址
 	 * @return JsonResult<String>
 	 */
+	@ApiOperation(value = "查询代码生成列表")
 	@PreAuthorize("@role.hasPermi('dfs:dfs:remove')")
 	@Log(service = "删除文件", businessType = BusinessType.DELETE)
 	@DeleteMapping("/{fileIds}")
@@ -72,6 +78,7 @@ public class DFSController extends TWTController {
 	 * @param sysDfs SysDfs
 	 * @return JsonResult<TableDataInfo>
 	 */
+	@ApiOperation(value = "分页查询")
 	@PreAuthorize("@role.hasPermi('dfs:dfs:list')")
 	@GetMapping("/pageQuery")
 	public JsonResult<TableDataInfo> pageQuery(SysDfs sysDfs) {
