7031d1c0064b431b6d30c5483534195415efa1ac
赵怡宁 <2547702006@qq.com>
2022-09-08 22:46:56 +0800

    fix:job实体类

--- twelvet-api/twelvet-api-gen/src/main/java/com/twelvet/api/gen/domain/GenTableColumn.java
+++ twelvet-api/twelvet-api-gen/src/main/java/com/twelvet/api/gen/domain/GenTableColumn.java
@@ -2,6 +2,8 @@ package com.twelvet.api.gen.domain;
 
 import com.twelvet.framework.core.application.domain.BaseEntity;
 import com.twelvet.framework.utils.StringUtils;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 
 import javax.validation.constraints.NotBlank;
 
@@ -10,6 +12,7 @@ import javax.validation.constraints.NotBlank;
  * @WebSite www.twelvet.cn
  * @Description: 代码生成业务字段表 gen_table_column
  */
+@ApiModel("代码生成业务字段表")
 public class GenTableColumn extends BaseEntity {
 
 	private static final long serialVersionUID = 1L;
@@ -17,92 +20,110 @@ public class GenTableColumn extends BaseEntity {
 	/**
 	 * 编号
 	 */
+	@ApiModelProperty(value = "编号")
 	private Long columnId;
 
 	/**
 	 * 归属表编号
 	 */
+	@ApiModelProperty(value = "归属表编号")
 	private Long tableId;
 
 	/**
 	 * 列名称
 	 */
+	@ApiModelProperty(value = "列名称")
 	private String columnName;
 
 	/**
 	 * 列描述
 	 */
+	@ApiModelProperty(value = "列描述")
 	private String columnComment;
 
 	/**
 	 * 列类型
 	 */
+	@ApiModelProperty(value = "列类型")
 	private String columnType;
 
 	/**
 	 * JAVA类型
 	 */
+	@ApiModelProperty(value = "JAVA类型")
 	private String javaType;
 
 	/**
 	 * JAVA字段名
 	 */
+	@ApiModelProperty(value = "JAVA字段名")
 	@NotBlank(message = "Java属性不能为空")
 	private String javaField;
 
 	/**
 	 * 是否主键（1是）
 	 */
+	@ApiModelProperty(value = "是否主键")
 	private String isPk;
 
 	/**
 	 * 是否自增（1是）
 	 */
+	@ApiModelProperty(value = "是否自增")
 	private String isIncrement;
 
 	/**
 	 * 是否必填（1是）
 	 */
+	@ApiModelProperty(value = "是否必填")
 	private String isRequired;
 
 	/**
 	 * 是否为插入字段（1是）
 	 */
+	@ApiModelProperty(value = "是否为插入字段")
 	private String isInsert;
 
 	/**
 	 * 是否编辑字段（1是）
 	 */
+	@ApiModelProperty(value = "是否编辑字段")
 	private String isEdit;
 
 	/**
 	 * 是否列表字段（1是）
 	 */
+	@ApiModelProperty(value = "是否列表字段")
 	private String isList;
 
 	/**
 	 * 是否查询字段（1是）
 	 */
+	@ApiModelProperty(value = "是否查询字段")
 	private String isQuery;
 
 	/**
 	 * 查询方式（EQ等于、NE不等于、GT大于、LT小于、LIKE模糊、BETWEEN范围）
 	 */
+	@ApiModelProperty(value = "查询方式")
 	private String queryType;
 
 	/**
 	 * 显示类型（input文本框、textarea文本域、select下拉框、checkbox复选框、radio单选框、datetime日期控件、image图片上传控件、upload文件上传控件、editor富文本控件）
 	 */
+	@ApiModelProperty(value = "显示类型")
 	private String htmlType;
 
 	/**
 	 * 字典类型
 	 */
+	@ApiModelProperty(value = "字典类型")
 	private String dictType;
 
 	/**
 	 * 排序
 	 */
+	@ApiModelProperty(value = "排序")
 	private Integer sort;
 
 	public void setColumnId(Long columnId) {
