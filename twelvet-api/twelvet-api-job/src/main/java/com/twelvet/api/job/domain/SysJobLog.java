e7c4450f12e22560d9c7ad33253045577b551aba
赵怡宁 <2547702006@qq.com>
2022-09-08 22:42:07 +0800

    fix:job实体类

--- twelvet-api/twelvet-api-job/src/main/java/com/twelvet/api/job/domain/SysJobLog.java
+++ twelvet-api/twelvet-api-job/src/main/java/com/twelvet/api/job/domain/SysJobLog.java
@@ -2,6 +2,8 @@ package com.twelvet.api.job.domain;
 
 import com.twelvet.framework.core.application.domain.BaseEntity;
 import com.twelvet.framework.utils.annotation.excel.Excel;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import org.apache.commons.lang3.builder.ToStringBuilder;
 import org.apache.commons.lang3.builder.ToStringStyle;
 
@@ -12,42 +14,52 @@ import java.util.Date;
  * @WebSite www.twelvet.cn
  * @Description: 定时任务调度日志表 sys_job_log
  */
+@ApiModel("定时任务调度日志表")
 public class SysJobLog extends BaseEntity {
 
 	private static final long serialVersionUID = 1L;
 
 	/** ID */
+	@ApiModelProperty(value = "日志序号")
 	@Excel(name = "日志序号")
 	private Long jobLogId;
 
 	/** 任务名称 */
+	@ApiModelProperty(value = "任务名称")
 	@Excel(name = "任务名称")
 	private String jobName;
 
 	/** 任务组名 */
+	@ApiModelProperty(value = "任务组名")
 	@Excel(name = "任务组名")
 	private String jobGroup;
 
 	/** 调用目标字符串 */
+	@ApiModelProperty(value = "调用目标字符串")
 	@Excel(name = "调用目标字符串")
 	private String invokeTarget;
 
 	/** 日志信息 */
+	@ApiModelProperty(value = "日志信息")
 	@Excel(name = "日志信息")
 	private String jobMessage;
 
 	/** 执行状态（0正常 1失败） */
+	@ApiModelProperty(value = "执行状态")
 	@Excel(name = "执行状态", readConverterExp = "1=正常,0=失败")
 	private String status;
 
 	/** 异常信息 */
+	@ApiModelProperty(value = "异常信息")
 	@Excel(name = "异常信息")
 	private String exceptionInfo;
 
 	/** 开始时间 */
+	@ApiModelProperty(value = "开始时间")
 	private Date startTime;
 
 	/** 停止时间 */
+	@ApiModelProperty(value = "停止时间")
 	private Date stopTime;
 
 	public Long getJobLogId() {
