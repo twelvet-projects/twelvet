d70a31286438816ce17f63c4d427f9b3724a9dc3
twelvet <2471835953@qq.com>
2022-09-22 00:24:39 +0800

    优化k8s

--- twelvet-framework/twelvet-framework-security/src/main/java/com/twelvet/framework/security/config/AuthIgnoreConfig.java
+++ twelvet-framework/twelvet-framework-security/src/main/java/com/twelvet/framework/security/config/AuthIgnoreConfig.java
@@ -1,17 +1,19 @@
 package com.twelvet.framework.security.config;
 
+import cn.hutool.core.util.ReUtil;
 import cn.hutool.extra.spring.SpringUtil;
 import com.twelvet.framework.security.annotation.AuthIgnore;
 import com.twelvet.framework.utils.$;
+import javassist.runtime.Inner;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.core.annotation.AnnotationUtils;
 import org.springframework.web.method.HandlerMethod;
 import org.springframework.web.servlet.mvc.method.RequestMappingInfo;
 import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
+import java.util.regex.Pattern;
 
 /**
  * @author twelvet
@@ -21,38 +23,40 @@ import java.util.Map;
 @ConfigurationProperties(prefix = "security.oauth2.ignore")
 public class AuthIgnoreConfig implements InitializingBean {
 
-	private List<String> urls = new ArrayList<>();
+    private static final Pattern PATTERN = Pattern.compile("\\{(.*?)\\}");
 
-	public List<String> getUrls() {
-		return urls;
-	}
+    private List<String> urls = new ArrayList<>();
 
-	public void setUrls(List<String> urls) {
-		this.urls = urls;
-	}
+    public List<String> getUrls() {
+        return urls;
+    }
 
-	/**
-	 * 重写bean注入后
-	 */
-	@Override
-	public void afterPropertiesSet() {
-		RequestMappingHandlerMapping mapping = SpringUtil.getBean("requestMappingHandlerMapping");
-		Map<RequestMappingInfo, HandlerMethod> map = mapping.getHandlerMethods();
+    public void setUrls(List<String> urls) {
+        this.urls = urls;
+    }
 
-		// 遍历所有mapping
-		map.keySet().forEach(mappingInfo -> {
+    /**
+     * 重写bean注入后
+     */
+    @Override
+    public void afterPropertiesSet() {
+        RequestMappingHandlerMapping mapping = SpringUtil.getBean("requestMappingHandlerMapping");
+        Map<RequestMappingInfo, HandlerMethod> map = mapping.getHandlerMethods();
 
-			HandlerMethod handlerMethod = map.get(mappingInfo);
-			// 检测方法是否存在注解
-			AuthIgnore method = handlerMethod.getMethod().getAnnotation(AuthIgnore.class);
-			// 检测Controller是否在注解
-			AuthIgnore controller = handlerMethod.getBeanType().getAnnotation(AuthIgnore.class);
+        // 遍历所有mapping
+        map.keySet().forEach(mappingInfo -> {
 
-			// 本方法或本Controller存在AuthIgnore注解将存进列表
-			if ($.isNotEmpty(method) || $.isNotEmpty(controller)) {
-				urls.addAll(mappingInfo.getPatternsCondition().getPatterns());
-			}
-		});
-	}
+            HandlerMethod handlerMethod = map.get(mappingInfo);
+            // 获取方法上边的注解 替代path variable 为 *
+            AuthIgnore method = AnnotationUtils.findAnnotation(handlerMethod.getMethod(), AuthIgnore.class);
+            Optional.ofNullable(method).ifPresent(inner -> Objects.requireNonNull(mappingInfo.getPathPatternsCondition())
+                    .getPatternValues().forEach(url -> urls.add(ReUtil.replaceAll(url, PATTERN, "*"))));
+
+            // 获取类上边的注解, 替代path variable 为 *
+            AuthIgnore controller = AnnotationUtils.findAnnotation(handlerMethod.getBeanType(), AuthIgnore.class);
+            Optional.ofNullable(controller).ifPresent(inner -> Objects.requireNonNull(mappingInfo.getPathPatternsCondition())
+                    .getPatternValues().forEach(url -> urls.add(ReUtil.replaceAll(url, PATTERN, "*"))));
+        });
+    }
 
 }
