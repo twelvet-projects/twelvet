2dd4bb12876a8a927e87897a8ced3317dd25fa86
TwelveT <2471835953@qq.com>
2022-11-14 17:17:04 +0800

    修复sentinel

--- twelvet-visual/twelvet-visual-monitor/src/main/java/com/twelvet/vosual/monitor/config/WebSecurityConfigurer.java
+++ twelvet-visual/twelvet-visual-monitor/src/main/java/com/twelvet/vosual/monitor/config/WebSecurityConfigurer.java
@@ -2,10 +2,8 @@ package com.twelvet.vosual.monitor.config;
 
 import de.codecentric.boot.admin.server.config.AdminServerProperties;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
 
@@ -23,18 +21,23 @@ public class WebSecurityConfigurer {
 		this.adminContextPath = adminServerProperties.getContextPath();
 	}
 
+	/**
+	 * spring security 默认的安全策略
+	 * @param http security注入点
+	 * @return SecurityFilterChain
+	 */
 	@Bean
-	public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+	SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
 		SavedRequestAwareAuthenticationSuccessHandler successHandler = new SavedRequestAwareAuthenticationSuccessHandler();
 		successHandler.setTargetUrlParameter("redirectTo");
 		successHandler.setDefaultTargetUrl(adminContextPath + "/");
-
-		return httpSecurity.headers().frameOptions().disable().and().authorizeRequests()
+		http.headers().frameOptions().disable().and().authorizeRequests()
 				.antMatchers(adminContextPath + "/assets/**", adminContextPath + "/login",
-						adminContextPath + "/actuator/**", adminContextPath + "/instances/**")
+						adminContextPath + "/instances/**", adminContextPath + "/actuator/**")
 				.permitAll().anyRequest().authenticated().and().formLogin().loginPage(adminContextPath + "/login")
 				.successHandler(successHandler).and().logout().logoutUrl(adminContextPath + "/logout").and().httpBasic()
-				.and().csrf().disable().build();
+				.and().csrf().disable();
+		return http.build();
 	}
 
 }
