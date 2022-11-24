66d9a14226dcfd048ae33b26c7098e109abd5b28
TwelveT <2471835953@qq.com>
2022-11-24 09:23:36 +0800

    升级Spring Authorization Server0.4.0

--- twelvet-auth/src/main/java/com/twelvet/auth/support/sms/OAuth2ResourceOwnerSmsAuthenticationProvider.java
+++ twelvet-auth/src/main/java/com/twelvet/auth/support/sms/OAuth2ResourceOwnerSmsAuthenticationProvider.java
@@ -2,38 +2,19 @@ package com.twelvet.auth.support.sms;
 
 import com.twelvet.auth.support.base.OAuth2ResourceOwnerBaseAuthenticationProvider;
 import com.twelvet.framework.core.constants.SecurityConstants;
-import com.twelvet.framework.security.utils.OAuth2ErrorCodesExpand;
-import com.twelvet.framework.security.utils.ScopeException;
-import org.apache.poi.ss.formula.functions.T;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.support.MessageSourceAccessor;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.AuthenticationException;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.oauth2.core.*;
-import org.springframework.security.oauth2.server.authorization.OAuth2Authorization;
+import org.springframework.security.oauth2.core.AuthorizationGrantType;
+import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
+import org.springframework.security.oauth2.core.OAuth2ErrorCodes;
+import org.springframework.security.oauth2.core.OAuth2Token;
 import org.springframework.security.oauth2.server.authorization.OAuth2AuthorizationService;
-import org.springframework.security.oauth2.server.authorization.authentication.OAuth2AccessTokenAuthenticationToken;
-import org.springframework.security.oauth2.server.authorization.authentication.OAuth2ClientAuthenticationToken;
 import org.springframework.security.oauth2.server.authorization.client.RegisteredClient;
-import org.springframework.security.oauth2.server.authorization.context.ProviderContextHolder;
-import org.springframework.security.oauth2.server.authorization.token.DefaultOAuth2TokenContext;
-import org.springframework.security.oauth2.server.authorization.token.OAuth2TokenContext;
 import org.springframework.security.oauth2.server.authorization.token.OAuth2TokenGenerator;
-import org.springframework.util.CollectionUtils;
 
-import java.security.Principal;
-import java.time.Instant;
-import java.util.LinkedHashSet;
 import java.util.Map;
-import java.util.Objects;
-import java.util.Set;
-import java.util.function.Supplier;
 
 /**
  * @author twelvet
