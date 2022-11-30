bd3f5867f0f82c93886a9694b693a65222db3a78
twelvet <2471835953@qq.com>
2022-11-30 18:13:10 +0800

    取消测试用例

--- twelvet-auth/src/main/java/com/twelvet/auth/endpoint/IndexController.java
+++ twelvet-auth/src/main/java/com/twelvet/auth/endpoint/IndexController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class IndexController {
 
+    // TODO 实现测试登录
 	/*@GetMapping("/")
 	public AjaxResult callback(OAuth2AuthenticationToken oAuth2AuthenticationToken) {
 		return AjaxResult.success(oAuth2AuthenticationToken);
