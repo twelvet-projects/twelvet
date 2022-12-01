bbb7b964797c22ad4f0cefad92066555f7be6b9c
TwelveT <2471835953@qq.com>
2022-12-01 13:47:44 +0800

    支持客户端模式用户储存

--- twelvet-framework/twelvet-framework-security/src/main/java/com/twelvet/framework/security/config/TWTCustomOpaqueTokenIntrospect.java
+++ twelvet-framework/twelvet-framework-security/src/main/java/com/twelvet/framework/security/config/TWTCustomOpaqueTokenIntrospect.java
@@ -5,15 +5,17 @@ import com.twelvet.framework.security.domain.LoginUser;
 import com.twelvet.framework.security.service.TwUserDetailsService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.Ordered;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.AuthorityUtils;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.oauth2.core.AuthorizationGrantType;
 import org.springframework.security.oauth2.core.OAuth2AuthenticatedPrincipal;
 import org.springframework.security.oauth2.server.authorization.OAuth2Authorization;
 import org.springframework.security.oauth2.server.authorization.OAuth2AuthorizationService;
 import org.springframework.security.oauth2.server.authorization.OAuth2TokenType;
+import org.springframework.security.oauth2.server.resource.InvalidBearerTokenException;
 import org.springframework.security.oauth2.server.resource.introspection.OpaqueTokenIntrospector;
 
 import java.security.Principal;
@@ -40,6 +42,15 @@ public class TWTCustomOpaqueTokenIntrospect implements OpaqueTokenIntrospector {
 	@Override
 	public OAuth2AuthenticatedPrincipal introspect(String token) {
 		OAuth2Authorization oldAuthorization = authorizationService.findByToken(token, OAuth2TokenType.ACCESS_TOKEN);
+		if (Objects.isNull(oldAuthorization)) {
+			throw new InvalidBearerTokenException(token);
+		}
+
+		// 客户端模式默认返回
+		if (AuthorizationGrantType.CLIENT_CREDENTIALS.equals(oldAuthorization.getAuthorizationGrantType())) {
+			return new TwClientCredentialsOAuth2AuthenticatedPrincipal(oldAuthorization.getAttributes(),
+					AuthorityUtils.NO_AUTHORITIES, oldAuthorization.getPrincipalName());
+		}
 
 		Map<String, TwUserDetailsService> userDetailsServiceMap = SpringUtil.getBeansOfType(TwUserDetailsService.class);
 
