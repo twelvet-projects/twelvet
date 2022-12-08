ebabf6fdb44449c14c5917cf4ea9542fa0c03d06
TwelveT <2471835953@qq.com>
2022-12-08 15:36:58 +0800

    Spring Cloud 2022.0.0-RC3

--- twelvet-framework/twelvet-framework-swagger/src/main/java/com/twelvet/framework/swagger/annotation/EnableTwelveTSwagger2.java
+++ twelvet-framework/twelvet-framework-swagger/src/main/java/com/twelvet/framework/swagger/annotation/EnableTwelveTSwagger2.java
@@ -1,6 +1,7 @@
 package com.twelvet.framework.swagger.annotation;
 
 
+import com.twelvet.framework.swagger.config.SwaggerAutoConfiguration;
 import com.twelvet.framework.swagger.config.SwaggerProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Import;
@@ -17,7 +18,7 @@ import java.lang.annotation.*;
 @Documented
 @Inherited
 @EnableConfigurationProperties(SwaggerProperties.class)
-//@Import({ SwaggerAutoConfiguration.class })
+@Import({ SwaggerAutoConfiguration.class })
 public @interface EnableTwelveTSwagger2 {
 
 }
