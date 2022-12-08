f1ddddd022be8f118a704abac8387bc2654dc936
TwelveT <2471835953@qq.com>
2022-12-08 18:11:58 +0800

    redisson

--- twelvet-framework/twelvet-framework-redis/src/main/java/com/twelvet/framework/redis/service/aspect/TwSynchronizedAspect.java
+++ twelvet-framework/twelvet-framework-redis/src/main/java/com/twelvet/framework/redis/service/aspect/TwSynchronizedAspect.java
@@ -1,4 +1,3 @@
-/*
 package com.twelvet.framework.redis.service.aspect;
 
 import com.twelvet.framework.redis.service.annotation.TwSynchronized;
@@ -19,13 +18,11 @@ import org.springframework.stereotype.Component;
 
 import java.lang.reflect.Method;
 
-*/
 /**
  * @author twelvet
  * @WebSite www.twelvet.cn
  * @Description: 注解分布式锁
- *//*
-
+ */
 @Aspect
 @Component
 public class TwSynchronizedAspect {
@@ -35,43 +32,35 @@ public class TwSynchronizedAspect {
 	@Autowired
 	private RedissonClient redissonClient;
 
-	*/
-/**
+	/**
 	 * 配置织入点
-	 *//*
-
+	 */
 	@Pointcut("@annotation(com.twelvet.framework.redis.service.annotation.TwSynchronized)")
 	public void synchronizedPointCut() {
 	}
 
-	*/
-/**
+	/**
 	 * 执行钱锁定
 	 * @param point JoinPoint
-	 *//*
-
+	 */
 	@Before("synchronizedPointCut()")
 	public void doBefore(JoinPoint point) {
 		handleLock(point);
 	}
 
-	*/
-/**
+	/**
 	 * 执行后解锁
 	 * @param point JoinPoint
-	 *//*
-
+	 */
 	@After("synchronizedPointCut()")
 	public void doAfter(JoinPoint point) {
 		handleUnLock(point);
 	}
 
-	*/
-/**
+	/**
 	 * 分布式锁try上锁Lock
 	 * @param joinPoint JoinPoint
-	 *//*
-
+	 */
 	protected void handleLock(final JoinPoint joinPoint) {
 		// 获得注解
 		TwSynchronized twSync = getAnnotationLog(joinPoint);
@@ -87,12 +76,10 @@ public class TwSynchronizedAspect {
 		log.info("cloud synchronized：Lock");
 	}
 
-	*/
-/**
+	/**
 	 * 分布式锁解锁Lock
 	 * @param joinPoint JoinPoint
-	 *//*
-
+	 */
 	protected void handleUnLock(final JoinPoint joinPoint) {
 		// 获得注解
 		TwSynchronized twSync = getAnnotationLog(joinPoint);
@@ -108,13 +95,11 @@ public class TwSynchronizedAspect {
 		log.info("cloud synchronized：UnLock");
 	}
 
-	*/
-/**
+	/**
 	 * 是否存在注解，如果存在就获取
 	 * @param joinPoint JoinPoint
 	 * @return 返回注解信息
-	 *//*
-
+	 */
 	private TwSynchronized getAnnotationLog(JoinPoint joinPoint) {
 		Signature signature = joinPoint.getSignature();
 		MethodSignature methodSignature = (MethodSignature) signature;
@@ -127,4 +112,3 @@ public class TwSynchronizedAspect {
 	}
 
 }
-*/
