6fa09fd37ce7a5fe84ad28449fe944c73228f91c
TwelveT <2471835953@qq.com>
2022-12-10 09:50:22 +0800

    成功/失败变量

--- twelvet-framework/twelvet-framework-core/src/main/java/com/twelvet/framework/core/constants/Constants.java
+++ twelvet-framework/twelvet-framework-core/src/main/java/com/twelvet/framework/core/constants/Constants.java
@@ -53,12 +53,12 @@ public class Constants {
 	/**
 	 * 成功标记
 	 */
-	public static final Integer SUCCESS = 200;
+	public static final Integer SUCCESS = 0;
 
 	/**
 	 * 失败标记
 	 */
-	public static final Integer FAIL = 500;
+	public static final Integer FAIL = 1;
 
 	/**
 	 * 登录成功
