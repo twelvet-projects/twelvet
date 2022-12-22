71ab34ceb8cad76e2e6804cf1d8940147130b300
twelvet <2471835953@qq.com>
2022-12-22 20:16:05 +0800

    放行api

--- twelvet-auth/src/main/java/com/twelvet/auth/config/AuthorizationServerConfiguration.java
+++ twelvet-auth/src/main/java/com/twelvet/auth/config/AuthorizationServerConfiguration.java
@@ -67,7 +67,7 @@ public class AuthorizationServerConfiguration {
 
 		DefaultSecurityFilterChain securityFilterChain = http.authorizeHttpRequests(authorizeRequests -> {
 			// 自定义接口、端点暴露
-			authorizeRequests.requestMatchers("/token/**", "/actuator/**", "/assets/**", "/error", "/v3/api-docs")
+			authorizeRequests.requestMatchers("/api/token/*","/token/**", "/actuator/**", "/assets/**", "/error", "/v3/api-docs")
 					.permitAll();
 			authorizeRequests.anyRequest().authenticated();
 		}).apply(authorizationServerConfigurer.authorizationService(authorizationService)// redis存储token的实现
