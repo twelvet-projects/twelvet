3d31e842fb81e7d5920ae61ce9e5a8d30fb9db75
twelvet <2471835953@qq.com>
2022-12-27 23:32:55 +0800

    采用Spring Cache实现缓存

--- twelvet-server/twelvet-server-system/src/main/java/com/twelvet/server/system/utils/DictUtils.java
+++ twelvet-server/twelvet-server-system/src/main/java/com/twelvet/server/system/utils/DictUtils.java
@@ -1,14 +1,10 @@
 package com.twelvet.server.system.utils;
 
-import com.alibaba.fastjson.JSONArray;
-import com.twelvet.api.system.domain.SysDictData;
 import com.twelvet.framework.redis.service.RedisService;
 import com.twelvet.framework.redis.service.constants.CacheConstants;
 import com.twelvet.framework.utils.SpringContextHolder;
-import com.twelvet.framework.utils.StringUtils;
 
 import java.util.Collection;
-import java.util.List;
 
 /**
  * @author twelvet
@@ -16,23 +12,6 @@ import java.util.List;
  * @Description: 字典工具类
  */
 public class DictUtils {
-
-	/**
-	 * 设置字典缓存
-	 * @param key 参数键
-	 * @param dictDatas 字典数据列表
-	 */
-	public static void setDictCache(String key, List<SysDictData> dictDatas) {
-		SpringContextHolder.getBean(RedisService.class).setCacheObject(getCacheKey(key), dictDatas);
-	}
-
-	/**
-	 * 删除指定字典缓存
-	 * @param key 字典键
-	 */
-	public static void removeDictCache(String key) {
-		SpringContextHolder.getBean(RedisService.class).deleteObject(getCacheKey(key));
-	}
 
 	/**
 	 * 清空字典缓存
