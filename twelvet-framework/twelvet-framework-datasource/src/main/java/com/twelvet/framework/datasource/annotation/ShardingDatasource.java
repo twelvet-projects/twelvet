dc8427d5a26b335f93492c72afc8047459929085
TwelveT <2471835953@qq.com>
2023-02-14 11:41:30 +0800

    切换hikari数据源，并加入ShardingJDBC数据源

--- twelvet-framework/twelvet-framework-datasource/src/main/java/com/twelvet/framework/datasource/annotation/ShardingDatasource.java
+++ twelvet-framework/twelvet-framework-datasource/src/main/java/com/twelvet/framework/datasource/annotation/ShardingDatasource.java
@@ -4,15 +4,16 @@ import com.baomidou.dynamic.datasource.annotation.DS;
 
 import java.lang.annotation.*;
 
+
 /**
  * @author twelvet
  * @WebSite www.twelvet.cn
- * @Description: 主库数据源
+ * @Description: 从库数据源
  */
 @Target({ ElementType.TYPE, ElementType.METHOD })
 @Retention(RetentionPolicy.RUNTIME)
 @Documented
-@DS("master")
-public @interface MasterDatasource {
+@DS("shardingSphere")
+public @interface ShardingDatasource {
 
 }
