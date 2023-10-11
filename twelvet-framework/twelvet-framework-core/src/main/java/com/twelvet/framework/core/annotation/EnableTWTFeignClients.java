40340cbd75234ffb07288e23420568b94f8bc0f3
TwelveT <2471835953@qq.com>
2023-10-11 17:03:08 +0800

    修正EnableTWTFeignClients的警告

--- twelvet-framework/twelvet-framework-core/src/main/java/com/twelvet/framework/core/annotation/EnableTWTFeignClients.java
+++ twelvet-framework/twelvet-framework-core/src/main/java/com/twelvet/framework/core/annotation/EnableTWTFeignClients.java
@@ -3,7 +3,9 @@ package com.twelvet.framework.core.annotation;
 import com.twelvet.framework.core.config.JavaTimeModule;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.cloud.openfeign.FeignAutoConfiguration;
+import org.springframework.cloud.openfeign.FeignClientsConfiguration;
 import org.springframework.context.annotation.Import;
+import org.springframework.core.annotation.AliasFor;
 
 import java.lang.annotation.*;
 
@@ -20,14 +22,24 @@ import java.lang.annotation.*;
 @EnableFeignClients
 public @interface EnableTWTFeignClients {
 
+	/**
+	 * Alias for the {@link #basePackages()} attribute. Allows for more concise annotation
+	 * declarations e.g.: {@code @ComponentScan("org.my.pkg")} instead of
+	 * {@code @ComponentScan(basePackages="org.my.pkg")}.
+	 * @return the array of 'basePackages'.
+	 */
 	String[] value() default {};
 
+	/**
+	 * Base packages to scan for annotated components.
+	 * <p>
+	 * {@link #value()} is an alias for (and mutually exclusive with) this attribute.
+	 * <p>
+	 * Use {@link #basePackageClasses()} for a type-safe alternative to String-based
+	 * package names.
+	 * @return the array of 'basePackages'.
+	 */
+	@AliasFor(annotation = EnableFeignClients.class, attribute = "basePackages")
 	String[] basePackages() default { "com.twelvet" };
-
-	Class<?>[] basePackageClasses() default {};
-
-	Class<?>[] defaultConfiguration() default {};
-
-	Class<?>[] clients() default {};
 
 }
