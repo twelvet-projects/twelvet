f1041135a3ae6dfc675c023a92f263cab8fcddf7
twelvet <2471835953@qq.com>
2023-11-05 19:42:48 +0800

    代码生成支持动态数据源

--- twelvet-server/twelvet-server-gen/src/main/java/com/twelvet/server/gen/service/impl/GenTableServiceImpl.java
+++ twelvet-server/twelvet-server-gen/src/main/java/com/twelvet/server/gen/service/impl/GenTableServiceImpl.java
@@ -28,11 +28,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.PlatformTransactionManager;
-import org.springframework.transaction.TransactionDefinition;
 import org.springframework.transaction.TransactionStatus;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.transaction.support.DefaultTransactionDefinition;
 import org.springframework.transaction.support.TransactionCallbackWithoutResult;
 import org.springframework.transaction.support.TransactionTemplate;
 
