d573e7f542c8b26675e79afffb4dfbb6b3b2aba6
TwelveT <2471835953@qq.com>
2023-11-16 11:10:32 +0800

    优化动态生成器

--- twelvet-server/twelvet-server-gen/src/main/java/com/twelvet/server/gen/mapper/GenMapper.java
+++ twelvet-server/twelvet-server-gen/src/main/java/com/twelvet/server/gen/mapper/GenMapper.java
@@ -1,6 +1,5 @@
 package com.twelvet.server.gen.mapper;
 
-
 import com.twelvet.api.gen.domain.GenTable;
 import com.twelvet.api.gen.domain.GenTableColumn;
 
@@ -13,25 +12,25 @@ import java.util.List;
  */
 public interface GenMapper {
 
-    /**
-     * 根据表名称查询列信息
-     * @param tableName 表名称
-     * @return 列信息
-     */
-    List<GenTableColumn> selectDbTableColumnsByName(String tableName);
+	/**
+	 * 根据表名称查询列信息
+	 * @param tableName 表名称
+	 * @return 列信息
+	 */
+	List<GenTableColumn> selectDbTableColumnsByName(String tableName);
 
-    /**
-     * 查询据库列表
-     * @param tableNames 表名称组
-     * @return 数据库表集合
-     */
-    List<GenTable> selectDbTableListByNames(String[] tableNames);
+	/**
+	 * 查询据库列表
+	 * @param tableNames 表名称组
+	 * @return 数据库表集合
+	 */
+	List<GenTable> selectDbTableListByNames(String[] tableNames);
 
-    /**
-     * 查询据库列表
-     * @param genTable 业务信息
-     * @return 数据库表集合
-     */
-    List<GenTable> selectDbTableList(GenTable genTable);
+	/**
+	 * 查询据库列表
+	 * @param genTable 业务信息
+	 * @return 数据库表集合
+	 */
+	List<GenTable> selectDbTableList(GenTable genTable);
 
 }
