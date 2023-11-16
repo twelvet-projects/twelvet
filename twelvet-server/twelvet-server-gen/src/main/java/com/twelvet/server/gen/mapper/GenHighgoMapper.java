d79c8df1656f74885142ed3191c197d17f68dcfd
TwelveT <2471835953@qq.com>
2023-11-16 14:54:43 +0800

    增加支持数据库

---
+++ twelvet-server/twelvet-server-gen/src/main/java/com/twelvet/server/gen/mapper/GenHighgoMapper.java
@@ -0,0 +1,10 @@
+package com.twelvet.server.gen.mapper;
+
+/**
+ * @author twelvet
+ * @WebSite www.twelvet.cn
+ * @Description: 支持Highgo代码生成器
+ */
+public interface GenHighgoMapper extends GenMapper {
+
+}
---
+++ twelvet-server/twelvet-server-gen/src/main/java/com/twelvet/server/gen/mapper/GenMssqlMapper.java
@@ -0,0 +1,10 @@
+package com.twelvet.server.gen.mapper;
+
+/**
+ * @author twelvet
+ * @WebSite www.twelvet.cn
+ * @Description: 支持Mssql代码生成器
+ */
+public interface GenMssqlMapper extends GenMapper {
+
+}
