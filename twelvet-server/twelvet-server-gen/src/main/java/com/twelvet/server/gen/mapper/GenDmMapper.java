41be54f621735523285508646acac3ac6d7b822c
TwelveT <2471835953@qq.com>
2023-11-16 14:54:49 +0800

    增加支持数据库

---
+++ twelvet-server/twelvet-server-gen/src/main/java/com/twelvet/server/gen/mapper/GenDmMapper.java
@@ -0,0 +1,10 @@
+package com.twelvet.server.gen.mapper;
+
+/**
+ * @author twelvet
+ * @WebSite www.twelvet.cn
+ * @Description: 支持达梦代码生成器
+ */
+public interface GenDmMapper extends GenMapper {
+
+}
---
+++ twelvet-server/twelvet-server-gen/src/main/java/com/twelvet/server/gen/mapper/GenOracleMapper.java
@@ -0,0 +1,10 @@
+package com.twelvet.server.gen.mapper;
+
+/**
+ * @author twelvet
+ * @WebSite www.twelvet.cn
+ * @Description: 支持Oracle代码生成器
+ */
+public interface GenOracleMapper extends GenMapper {
+
+}
