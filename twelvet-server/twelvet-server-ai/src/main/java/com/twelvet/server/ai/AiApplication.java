808496193d57ccb8769ef490e575d63d5b366415
twelvet <2471835953@qq.com>
2024-10-26 18:28:38 +0800

    [add] twelvet-server-ai

---
+++ twelvet-server/twelvet-server-ai/src/main/java/com/twelvet/server/ai/AiApplication.java
@@ -0,0 +1,28 @@
+package com.twelvet.server.ai;
+
+import com.twelvet.framework.openfeign.annotation.EnableTWTFeignClients;
+import com.twelvet.framework.core.annotation.EnableTwelveTConfig;
+import com.twelvet.framework.security.annotation.EnableTWTResourceServer;
+import com.twelvet.framework.swagger.annotation.EnableTwelveTSwagger2;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author twelvet
+ * @WebSite twelvet.cn
+ * @Description: 启动程序
+ */
+@EnableTwelveTSwagger2
+@EnableTWTResourceServer
+@MapperScan("com.twelvet.**.mapper")
+@EnableTwelveTConfig
+@EnableTWTFeignClients
+@SpringBootApplication
+public class AiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiApplication.class, args);
+	}
+
+}
