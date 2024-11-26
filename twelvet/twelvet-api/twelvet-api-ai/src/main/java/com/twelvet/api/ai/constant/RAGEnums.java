4272a2af08f402b20aa74012785b69f050faf74a
twelvet <2471835953@qq.com>
2024-11-26 21:14:39 +0800

    :art: javaformat

--- twelvet/twelvet-api/twelvet-api-ai/src/main/java/com/twelvet/api/ai/constant/RAGEnums.java
+++ twelvet/twelvet-api/twelvet-api-ai/src/main/java/com/twelvet/api/ai/constant/RAGEnums.java
@@ -9,32 +9,32 @@ package com.twelvet.api.ai.constant;
  */
 public class RAGEnums {
 
-    /**
-     * 储存向量metadata枚举
-     */
-    public enum VectorMetadataEnums {
-        MODEL_ID("modelId", "知识库ID"),
-        DOC_ID("docId", "文档ID"),
-        SLICE_ID("sliceId", "切片ID"),
+	/**
+	 * 储存向量metadata枚举
+	 */
+	public enum VectorMetadataEnums {
 
-        ;
+		MODEL_ID("modelId", "知识库ID"), DOC_ID("docId", "文档ID"), SLICE_ID("sliceId", "切片ID"),
 
-        private final String code;
+		;
 
-        private final String desc;
+		private final String code;
 
-        VectorMetadataEnums(String code, String desc) {
-            this.code = code;
-            this.desc = desc;
-        }
+		private final String desc;
 
-        public String getCode() {
-            return code;
-        }
+		VectorMetadataEnums(String code, String desc) {
+			this.code = code;
+			this.desc = desc;
+		}
 
-        public String getDesc() {
-            return desc;
-        }
-    }
+		public String getCode() {
+			return code;
+		}
+
+		public String getDesc() {
+			return desc;
+		}
+
+	}
 
 }
