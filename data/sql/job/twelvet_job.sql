460738424544e6a28248cc62e030f62e2443e12b
twelvet <2471835953@qq.com>
2024-12-18 10:18:49 +0800

    :zap: 修正字符集

--- data/sql/job/twelvet_job.sql
+++ data/sql/job/twelvet_job.sql
@@ -218,24 +218,24 @@ create table QRTZ_SIMPROP_TRIGGERS
 
 CREATE TABLE `sys_job`
 (
-    `job_id`          bigint(20)                                              NOT NULL AUTO_INCREMENT COMMENT '任务ID',
-    `job_name`        varchar(64) CHARACTER SET utf8 COLLATE utf8_general_ci  NOT NULL DEFAULT '' COMMENT '任务名称',
-    `job_group`       varchar(64) CHARACTER SET utf8 COLLATE utf8_general_ci  NOT NULL DEFAULT 'DEFAULT' COMMENT '任务组名',
-    `invoke_target`   varchar(500) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '调用目标字符串',
-    `cron_expression` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL     DEFAULT '' COMMENT 'cron执行表达式',
-    `misfire_policy`  tinyint(1)                                              NULL     DEFAULT NULL COMMENT '计划执行错误策略（1立即执行 2执行一次 3放弃执行）',
-    `concurrent`      tinyint(1)                                              NULL     DEFAULT NULL COMMENT '是否并发执行（0允许 1禁止）',
-    `status`          char(1) CHARACTER SET utf8 COLLATE utf8_general_ci      NULL     DEFAULT '0' COMMENT '状态（0正常 1暂停）',
-    `create_by`       varchar(64) CHARACTER SET utf8 COLLATE utf8_general_ci  NULL     DEFAULT '' COMMENT '创建者',
-    `create_time`     datetime                                                NULL     DEFAULT NULL COMMENT '创建时间',
-    `update_by`       varchar(64) CHARACTER SET utf8 COLLATE utf8_general_ci  NULL     DEFAULT '' COMMENT '更新者',
-    `update_time`     datetime                                                NULL     DEFAULT NULL COMMENT '更新时间',
-    `remark`          varchar(500) CHARACTER SET utf8 COLLATE utf8_general_ci NULL     DEFAULT '' COMMENT '备注信息',
+    `job_id`          bigint(20)                                                    NOT NULL AUTO_INCREMENT COMMENT '任务ID',
+    `job_name`        varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci  NOT NULL DEFAULT '' COMMENT '任务名称',
+    `job_group`       varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci  NOT NULL DEFAULT 'DEFAULT' COMMENT '任务组名',
+    `invoke_target`   varchar(500) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '调用目标字符串',
+    `cron_expression` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL     DEFAULT '' COMMENT 'cron执行表达式',
+    `misfire_policy`  tinyint(1)                                                    NULL     DEFAULT NULL COMMENT '计划执行错误策略（1立即执行 2执行一次 3放弃执行）',
+    `concurrent`      tinyint(1)                                                    NULL     DEFAULT NULL COMMENT '是否并发执行（0允许 1禁止）',
+    `status`          char(1) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci      NULL     DEFAULT '0' COMMENT '状态（0正常 1暂停）',
+    `create_by`       varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci  NULL     DEFAULT '' COMMENT '创建者',
+    `create_time`     datetime                                                      NULL     DEFAULT NULL COMMENT '创建时间',
+    `update_by`       varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci  NULL     DEFAULT '' COMMENT '更新者',
+    `update_time`     datetime                                                      NULL     DEFAULT NULL COMMENT '更新时间',
+    `remark`          varchar(500) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL     DEFAULT '' COMMENT '备注信息',
     PRIMARY KEY (`job_id`, `job_name`, `job_group`) USING BTREE
 ) ENGINE = InnoDB
   AUTO_INCREMENT = 107
-  CHARACTER SET = utf8
-  COLLATE = utf8_general_ci COMMENT = '定时任务调度表'
+  CHARACTER SET = utf8mb4
+  COLLATE = utf8mb4_general_ci COMMENT = '定时任务调度表'
   ROW_FORMAT = Dynamic;
 
 -- ----------------------------
