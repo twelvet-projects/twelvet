cdff32c283496e6b9fc6a51c31f3e948fab30781
twelvet <2471835953@qq.com>
2025-05-17 07:59:14 +0800

    :sparkles: Nacos v3.0.0

--- twelvet-nacos/src/main/java/com/alibaba/nacos/NacosApplication.java
+++ twelvet-nacos/src/main/java/com/alibaba/nacos/NacosApplication.java
@@ -1,11 +1,15 @@
 package com.alibaba.nacos;
 
-import com.alibaba.nacos.console.config.ConfigConstants;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.boot.SpringApplication;
+import com.alibaba.nacos.console.NacosConsole;
+import com.alibaba.nacos.core.listener.startup.NacosStartUp;
+import com.alibaba.nacos.core.listener.startup.NacosStartUpManager;
+import com.alibaba.nacos.sys.env.Constants;
+import com.alibaba.nacos.sys.env.DeploymentType;
+import com.alibaba.nacos.sys.env.EnvUtil;
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ConfigurableApplicationContext;
 
 /**
  * @author twelvet
@@ -13,26 +17,38 @@ import org.springframework.scheduling.annotation.EnableScheduling;
  * @Description: nacos console 源码运行，方便开发 生产从官网下载zip最新版集群配置运行
  * https://github.com/alibaba/nacos/releases
  */
-@EnableScheduling
 @SpringBootApplication
 public class NacosApplication {
 
-	public static void main(String[] args) {
-		if (initEnv()) {
-			SpringApplication.run(NacosApplication.class, args);
-		}
-	}
-
 	/**
-	 * 初始化运行环境
+	 * 独立模式系统属性名称
 	 */
-	private static boolean initEnv() {
-		System.setProperty(ConfigConstants.STANDALONE_MODE, "true");
-		System.setProperty(ConfigConstants.AUTH_ENABLED, "true");
-		System.setProperty(ConfigConstants.LOG_BASEDIR, "logs");
-		System.setProperty(ConfigConstants.LOG_ENABLED, "false");
-		System.setProperty(ConfigConstants.NACOS_CONTEXT_PATH, "/nacos");
-		return true;
+	private static final String STANDALONE_MODE = "nacos.standalone";
+
+	public static void main(String[] args) {
+		System.setProperty(STANDALONE_MODE, "true");
+
+		String type = System.getProperty(Constants.NACOS_DEPLOYMENT_TYPE, Constants.NACOS_DEPLOYMENT_TYPE_MERGED);
+		DeploymentType deploymentType = DeploymentType.getType(type);
+		EnvUtil.setDeploymentType(deploymentType);
+
+		// Start Core Context
+		NacosStartUpManager.start(NacosStartUp.CORE_START_UP_PHASE);
+		ConfigurableApplicationContext coreContext = new SpringApplicationBuilder(NacosServerBasicApplication.class)
+			.web(WebApplicationType.NONE)
+			.run(args);
+
+		// Start Server Web Context
+		NacosStartUpManager.start(NacosStartUp.WEB_START_UP_PHASE);
+		ConfigurableApplicationContext serverWebContext = new SpringApplicationBuilder(NacosServerWebApplication.class)
+			.parent(coreContext)
+			.run(args);
+
+		// Start Console Context
+		NacosStartUpManager.start(NacosStartUp.CONSOLE_START_UP_PHASE);
+		ConfigurableApplicationContext consoleContext = new SpringApplicationBuilder(NacosConsole.class)
+			.parent(coreContext)
+			.run(args);
 	}
 
 }
