d3ec9ffdd1d335153ddaa9412c727eaaa5b53b1f
twelvet <2471835953@qq.com>
2025-05-22 08:08:39 +0800

    :sparkles: Spring AI v1.0.0

--- twelvet-server/twelvet-server-ai/src/main/java/com/twelvet/server/ai/config/VectorStoreConfiguration.java
+++ twelvet-server/twelvet-server-ai/src/main/java/com/twelvet/server/ai/config/VectorStoreConfiguration.java
@@ -15,7 +15,6 @@ import org.springframework.jdbc.core.JdbcTemplate;
 
 import javax.sql.DataSource;
 
-
 /**
  * 注入向量数据库能力
  *
@@ -26,34 +25,36 @@ import javax.sql.DataSource;
 @Configuration
 public class VectorStoreConfiguration {
 
-    // TODO 动态注册
-    @Value("${spring.ai.dashscope.api-key}")
-    private String aiKey;
+	// TODO 动态注册
+	@Value("${spring.ai.dashscope.api-key}")
+	private String aiKey;
 
-    /**
-     * 注册向量数据库
-     * @param jdbcTemplate JdbcTemplate
-     * @return VectorStore
-     */
-    @Bean
-    public VectorStore vectorStore(JdbcTemplate jdbcTemplate) {
-        DynamicRoutingDataSource dynamicRoutingDataSource = SpringContextHolder.getBean(DynamicRoutingDataSource.class);
-        DataSource dataSource = dynamicRoutingDataSource.getDataSource(AIDataSourceConstants.DS_VECTOR);
-        jdbcTemplate.setDataSource(dataSource);
+	/**
+	 * 注册向量数据库
+	 * @param jdbcTemplate JdbcTemplate
+	 * @return VectorStore
+	 */
+	@Bean
+	public VectorStore vectorStore(JdbcTemplate jdbcTemplate) {
+		DynamicRoutingDataSource dynamicRoutingDataSource = SpringContextHolder.getBean(DynamicRoutingDataSource.class);
+		DataSource dataSource = dynamicRoutingDataSource.getDataSource(AIDataSourceConstants.DS_VECTOR);
+		jdbcTemplate.setDataSource(dataSource);
 
-        // TODO 需要动态注册
-        DashScopeApi dashScopeApi = new DashScopeApi(aiKey);
-        EmbeddingModel embeddingModel = new DashScopeEmbeddingModel(dashScopeApi);
-        // TODO 需要动态注册
-        return PgVectorStore.builder(jdbcTemplate, embeddingModel)
-                .dimensions(1536)                    // Optional: defaults to model dimensions or 1536
-                .distanceType(PgVectorStore.PgDistanceType.COSINE_DISTANCE)       // Optional: defaults to COSINE_DISTANCE
-                .indexType(PgVectorStore.PgIndexType.HNSW)                     // Optional: defaults to HNSW
-                .initializeSchema(true)              // Optional: defaults to false
-                .schemaName("public")                // Optional: defaults to "public"
-                .vectorTableName("ai_vector")     // Optional: defaults to "vector_store"
-                .maxDocumentBatchSize(10000)         // Optional: defaults to 10000
-                .build();
-    }
+		// TODO 需要动态注册
+		DashScopeApi dashScopeApi = new DashScopeApi(aiKey);
+		EmbeddingModel embeddingModel = new DashScopeEmbeddingModel(dashScopeApi);
+		// TODO 需要动态注册
+		return PgVectorStore.builder(jdbcTemplate, embeddingModel)
+			.dimensions(1536) // Optional: defaults to model dimensions or 1536
+			.distanceType(PgVectorStore.PgDistanceType.COSINE_DISTANCE) // Optional:
+																		// defaults to
+																		// COSINE_DISTANCE
+			.indexType(PgVectorStore.PgIndexType.HNSW) // Optional: defaults to HNSW
+			.initializeSchema(true) // Optional: defaults to false
+			.schemaName("public") // Optional: defaults to "public"
+			.vectorTableName("ai_vector") // Optional: defaults to "vector_store"
+			.maxDocumentBatchSize(10000) // Optional: defaults to 10000
+			.build();
+	}
 
 }
