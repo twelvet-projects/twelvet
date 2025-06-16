1666ad2d317b0ef720d607e855e1ff7bb00eff26
twelvet <2471835953@qq.com>
2025-06-16 08:52:53 +0800

    :sparkles: 向量模型、排序模型

--- twelvet-server/twelvet-server-ai/src/main/java/com/twelvet/server/ai/model/ModelService.java
+++ twelvet-server/twelvet-server-ai/src/main/java/com/twelvet/server/ai/model/ModelService.java
@@ -1,8 +1,11 @@
 package com.twelvet.server.ai.model;
 
+import com.alibaba.cloud.ai.model.RerankModel;
 import com.twelvet.api.ai.constant.ModelEnums;
 import com.twelvet.api.ai.domain.AiModel;
+import com.twelvet.framework.core.exception.TWTException;
 import org.springframework.ai.chat.model.ChatModel;
+import org.springframework.ai.embedding.EmbeddingModel;
 import org.springframework.core.Ordered;
 
 /**
@@ -12,22 +15,43 @@ import org.springframework.core.Ordered;
  * @WebSite twelvet.cn
  * @date 2025-06-14
  */
-public interface ModelService extends Ordered {
+public abstract class ModelService implements Ordered {
 
 	/**
 	 * 重写此方法，以此判断是否支持此供应商
 	 * @param modelProviderEnums 目标客户端
 	 * @return true/false
 	 */
-	default boolean support(ModelEnums.ModelProviderEnums modelProviderEnums) {
-		return true;
-	}
+	public abstract boolean support(ModelEnums.ModelProviderEnums modelProviderEnums);
 
 	/**
 	 * 获取聊天模型
 	 * @param aiModel AiModel
 	 * @return ChatModel
 	 */
-	ChatModel getChatModel(AiModel aiModel);
+	public ChatModel getChatModel(AiModel aiModel) {
+		throw new TWTException(
+				String.format("%s does not support this ChatModel", aiModel.getModelSupplier().getDesc()));
+	}
+
+	/**
+	 * 获取向量模型
+	 * @param aiModel AiModel
+	 * @return ChatModel
+	 */
+	public EmbeddingModel getEmbeddingModel(AiModel aiModel) {
+		throw new TWTException(
+				String.format("%s does not support this EmbeddingModel", aiModel.getModelSupplier().getDesc()));
+	}
+
+	/**
+	 * 获取排序模型
+	 * @param aiModel AiModel
+	 * @return ChatModel
+	 */
+	public RerankModel getRerankModel(AiModel aiModel) {
+		throw new TWTException(
+				String.format("%s does not support this RerankModel", aiModel.getModelSupplier().getDesc()));
+	}
 
 }
--- twelvet-server/twelvet-server-ai/src/main/java/com/twelvet/server/ai/model/impl/DashScopModelServiceImpl.java
+++ twelvet-server/twelvet-server-ai/src/main/java/com/twelvet/server/ai/model/impl/DashScopModelServiceImpl.java
@@ -3,10 +3,17 @@ package com.twelvet.server.ai.model.impl;
 import com.alibaba.cloud.ai.dashscope.api.DashScopeApi;
 import com.alibaba.cloud.ai.dashscope.chat.DashScopeChatModel;
 import com.alibaba.cloud.ai.dashscope.chat.DashScopeChatOptions;
+import com.alibaba.cloud.ai.dashscope.embedding.DashScopeEmbeddingModel;
+import com.alibaba.cloud.ai.dashscope.embedding.DashScopeEmbeddingOptions;
+import com.alibaba.cloud.ai.dashscope.rerank.DashScopeRerankModel;
+import com.alibaba.cloud.ai.dashscope.rerank.DashScopeRerankOptions;
+import com.alibaba.cloud.ai.model.RerankModel;
 import com.twelvet.api.ai.constant.ModelEnums;
 import com.twelvet.api.ai.domain.AiModel;
 import com.twelvet.server.ai.model.ModelService;
 import org.springframework.ai.chat.model.ChatModel;
+import org.springframework.ai.document.MetadataMode;
+import org.springframework.ai.embedding.EmbeddingModel;
 import org.springframework.stereotype.Component;
 
 /**
@@ -17,7 +24,7 @@ import org.springframework.stereotype.Component;
  * @date 2025-06-14
  */
 @Component
-public class DashScopModelServiceImpl implements ModelService {
+public class DashScopModelServiceImpl extends ModelService {
 
 	@Override
 	public boolean support(ModelEnums.ModelProviderEnums modelProviderEnums) {
@@ -47,6 +54,37 @@ public class DashScopModelServiceImpl implements ModelService {
 		 * dashScopeChatOptions.setEnableSearch(Boolean.TRUE); }
 		 */
 		return DashScopeChatModel.builder().dashScopeApi(dashScopeApi).defaultOptions(dashScopeChatOptions).build();
+	}
+
+	/**
+	 * 获取项链模型
+	 * @param aiModel AiModel
+	 * @return ChatModel
+	 */
+	@Override
+	public EmbeddingModel getEmbeddingModel(AiModel aiModel) {
+		DashScopeApi dashScopeApi = DashScopeApi.builder().apiKey(aiModel.getApiKey()).build();
+		DashScopeEmbeddingOptions dashScopeEmbeddingOptions = DashScopeEmbeddingOptions.builder()
+			.withModel(aiModel.getModel())
+			.build();
+		return new DashScopeEmbeddingModel(dashScopeApi, MetadataMode.EMBED, dashScopeEmbeddingOptions);
+	}
+
+	/**
+	 * 获取排序模型
+	 * @param aiModel AiModel
+	 * @return ChatModel
+	 */
+	@Override
+	public RerankModel getRerankModel(AiModel aiModel) {
+		DashScopeApi dashScopeApi = DashScopeApi.builder()
+				.baseUrl(aiModel.getBaseUrl())
+				.apiKey(aiModel.getApiKey())
+				.build();
+		DashScopeRerankOptions scopeRerankOptions = DashScopeRerankOptions.builder()
+				.withModel(aiModel.getModel())
+				.build();
+		return new DashScopeRerankModel(dashScopeApi, scopeRerankOptions);
 	}
 
 	/**
