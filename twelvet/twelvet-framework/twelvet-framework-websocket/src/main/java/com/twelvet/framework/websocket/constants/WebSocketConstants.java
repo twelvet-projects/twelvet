aa5fb4aeab0ccbf1015781e47d6358302ad9d965
twelvet <2471835953@qq.com>
2025-08-11 10:47:50 +0800

    :sparkles: websocket

--- twelvet/twelvet-framework/twelvet-framework-websocket/src/main/java/com/twelvet/framework/websocket/constants/WebSocketConstants.java
+++ twelvet/twelvet-framework/twelvet-framework-websocket/src/main/java/com/twelvet/framework/websocket/constants/WebSocketConstants.java
@@ -1,10 +1,19 @@
 package com.twelvet.framework.websocket.constants;
 
+/**
+ * <p>
+ * 常量
+ * </p>
+ *
+ * @author TwelveT
+ * @WebSite twelvet.cn
+ * @date 2025-08-11
+ */
 public interface WebSocketConstants {
 
-    /**
-     * websocket订阅通道key
-     */
-    String CHANNEL = "websocket::channel::";
+	/**
+	 * websocket订阅通道key
+	 */
+	String CHANNEL = "websocket::channel::";
 
 }
